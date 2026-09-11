eff72524859291c9614c5613b2ba56c14ab6dd84
Steven Grose <600013496@live.wcs.ac.uk>
2026-09-11 12:02:37 +0100

    done

---
+++ Assets/Task2/Task2Scripts/PlayerControllerTask2.cs
@@ -0,0 +1,30 @@
+using JetBrains.Annotations;
+using TMPro;
+using UnityEngine;
+
+public class PlayerControllerTask2 : MonoBehaviour
+{
+    [SerializeField] int score = 0;
+    [SerializeField] bool hasFinishedLevel = false;
+    [SerializeField] float timer = 60.0f;
+
+    [SerializeField] TextMeshProUGUI ScoreTextBox;
+    [SerializeField] TextMeshProUGUI TimerTextBox;
+    [SerializeField] TextMeshProUGUI CompletionTextBox;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
